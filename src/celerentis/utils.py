458aca2680e2779387daa6fc693fa36d409fdd3e
rinor-ab <99rinor@gmail.com>
2025-08-30 22:38:22 +0200

    Update utils.py

--- src/celerentis/utils.py
+++ src/celerentis/utils.py
@@ -1,14 +1,11 @@
 from __future__ import annotations
 
-from typing import Iterable
+from collections.abc import Iterable
+
 from pptx.shapes.base import BaseShape
 
 GROUP = 6  # python-pptx shape type for groups
 
-def iter_all_shapes(shapes: Iterable[BaseShape]):
-    """Yield all shapes, recursively flattening grouped shapes."""
-    for shape in shapes:
-        yield from _iter_shape(shape)
 
 def _iter_shape(shape: BaseShape):
     if getattr(shape, "shape_type", None) == GROUP and hasattr(shape, "shapes"):
@@ -16,3 +13,9 @@ def _iter_shape(shape: BaseShape):
             yield from _iter_shape(s)
     else:
         yield shape
+
+
+def iter_all_shapes(shapes: Iterable[BaseShape]):
+    """Yield all shapes, recursively flattening grouped shapes."""
+    for shape in shapes:
+        yield from _iter_shape(shape)
